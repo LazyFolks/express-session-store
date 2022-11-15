ed976130897cc3e9e13664990b9198b06f15179c
vkartk <53650724+vkartk@users.noreply.github.com>
2022-11-15 23:39:58 +0530

    Add generateSessionId() & generateSessionUUId() in main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 const redisSession = require('./session/redisSession')
-const { session, expressSession } = require('./session/expressSession')
+const { session, expressSession, generateSessionId, generateSessionUUId } = require('./session/expressSession')
 
 module.exports = {
     session:session,
+    generateSessionId:generateSessionId,
+    generateSessionUUId:generateSessionUUId,
     expressSession:expressSession,
     redisSession:redisSession
 }
